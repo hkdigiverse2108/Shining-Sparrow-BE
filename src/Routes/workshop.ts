f2b7dbd81f66cf34e5505caeae694eb8d890e16f
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-06-23 14:26:45 +0530

    Workshop purchase problem solved

--- src/Routes/workshop.ts
+++ src/Routes/workshop.ts
@@ -9,7 +9,7 @@ router.post('/edit', adminJWT, workshopController.edit_workshop_by_id);
 router.delete('/delete/:id', adminJWT, workshopController.delete_workshop_by_id);
 router.get('/all', userJWT, workshopController.get_all_workshop);
 router.get('/my-workshops', userJWT, workshopController.get_my_workshops);
-router.post('/purchase', adminJWT, workshopController.purchase_workshop);
+router.post('/purchase', userJWT, workshopController.purchase_workshop);
 router.get('/:id', userJWT, workshopController.get_workshop_by_id);
 
 export const workshopRoute = router;
