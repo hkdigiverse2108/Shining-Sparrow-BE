bb459bff66a66108ea8d8cdbc29a68afc28b158d
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-06-26 10:26:18 +0530

    access update for testimonial.

--- src/Routes/testimonial.ts
+++ src/Routes/testimonial.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import { testimonialController } from '../controllers';
-import { adminJWT, userJWTOptional } from '../helper';
+import { adminJWT, userJWT, userJWTOptional } from '../helper';
 
 const router = express.Router();
 
-router.post('/add', adminJWT, testimonialController.add_testimonial);
+router.post('/add', userJWT, testimonialController.add_testimonial);
 router.post('/edit', adminJWT, testimonialController.edit_testimonial_by_id);
 router.delete('/delete/:id', adminJWT, testimonialController.delete_testimonial_by_id);
 router.get('/all', userJWTOptional, testimonialController.get_all_testimonial);
